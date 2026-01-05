7f3aee2db0b62c6451e5eafeeeac1f1b8915bb87
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-01-05 16:33:23 +0200

    Refactor ServerConnectionManager and MessageRouter for improved message handling and connection management

--- src/main/java/com/boredxgames/tictactoeclient/domain/network/ServerConnectionManager.java
+++ src/main/java/com/boredxgames/tictactoeclient/domain/network/ServerConnectionManager.java
@@ -5,83 +5,86 @@
 package com.boredxgames.tictactoeclient.domain.network;
 
 import com.boredxgames.tictactoeclient.domain.services.communication.Message;
+import com.boredxgames.tictactoeclient.domain.services.communication.MessageRouter;
+import com.google.gson.Gson;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+
 /**
  *
  * @author Hazem
  */
 public class ServerConnectionManager {
-    private Socket socket; 
-    private DataInputStream dis  ; 
-    private DataOutputStream dos ;
-    private Thread th ; 
-    private static ServerConnectionManager instance ;
+
+    private Socket socket;
+    private DataInputStream dis;
+    private DataOutputStream dos;
+    private Thread th;
+    private Gson gson;
+    private static ServerConnectionManager instance;
     
+
     private ServerConnectionManager() {
     }
-  
-    public synchronized ServerConnectionManager getInstance()
-    {
-        if(instance==null)
-        {
-            instance=new ServerConnectionManager();
+
+    public static ServerConnectionManager getInstance() {
+        if (instance == null) {
+            instance = new ServerConnectionManager();
         }
-        
+
         return instance;
     }
-    
-    public void connect(String host , int port) throws IOException
-    {
-        socket = new Socket( host ,  port);
-        dis= new DataInputStream(socket.getInputStream());
+
+    public void connect(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        dis = new DataInputStream(socket.getInputStream());
         dos = new DataOutputStream(socket.getOutputStream());
         th = new Thread(this::readMessages);
         th.start();
     }
-    
-    private void readMessages()
-    {
-       
-            while (!socket.isClosed()) {
-                try {
-                    String jsonResponse = dis.readUTF();
-                } catch (IOException ex) {
-                    System.getLogger(ServerConnectionManager.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
-                }
-                
-            }
-       
-    }
-    
-    
-    public synchronized void sendMessage(Message msg) throws IOException
-    {
-        String jsonMessage =null ;
-        if(jsonMessage!=null)
-        dos.writeUTF(jsonMessage.toString());
-        dos.flush();
-        
-    }
-    
-    public void close (){
-        try{
-        if(socket!=null) {
-            socket.close();
-        }
-        if(dis!=null) {
-            dis.close();
-        }
-        if(dos!=null) {
-            dos.close();
-        }
-    }
-        catch (IOException ex) {
+
+    private void readMessages() {
+
+        while (!socket.isClosed()) {
+            try {
+                String response = dis.readUTF();
+                Message message = gson.fromJson(response, Message.class);
+                MessageRouter router = MessageRouter.getInstance();
+                router.navigateMessage(message);
+            } catch (IOException ex) {
                 System.getLogger(ServerConnectionManager.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
             }
-        
+
+        }
+
+    }
+
+    public synchronized void sendMessage(Message msg) throws IOException {
+        String jsonMessage = null;
+        if (jsonMessage != null) {
+            dos.writeUTF(jsonMessage);
+        }
+        dos.flush();
+
+    }
+
+    public void close() {
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+            if (dis != null) {
+                dis.close();
+            }
+            if (dos != null) {
+                dos.close();
+            }
+        } catch (IOException ex) {
+            System.getLogger(ServerConnectionManager.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+
        
     }
 }
--- src/main/java/com/boredxgames/tictactoeclient/domain/services/communication/MessageRouter.java
+++ src/main/java/com/boredxgames/tictactoeclient/domain/services/communication/MessageRouter.java
@@ -9,12 +9,15 @@ package com.boredxgames.tictactoeclient.domain.services.communication;
  * @author Hazem
  */
 
-import org.json.JSONObject;
+import com.boredxgames.tictactoeclient.domain.network.ServerConnectionManager;
+import static com.boredxgames.tictactoeclient.domain.services.communication.Action.LOGIN;
+import static com.boredxgames.tictactoeclient.domain.services.communication.Action.REGISTER;
 
 public class MessageRouter {
     private static MessageRouter instance;
-
+    private static ServerConnectionManager connection;
     private MessageRouter() {
+        connection = ServerConnectionManager.getInstance();
     }
 
     public static MessageRouter getInstance() {
@@ -24,12 +27,46 @@ public class MessageRouter {
         return instance;
     }
 
-    public void navigateMessage(String message)  {
+    public void navigateMessage(Message message){
+       MessageType messageType = message.getHeader().getMsgType();
        
+       assert messageType!=null; 
+       Message responseMessage;
+        switch (messageType) {
+            case REQUEST:
+                 responseMessage = handleRequest(message);
+                break;
+            case RESPONSE:
+                 responseMessage = handleResponse(message);
+                break;
+            case EVENT:
+                // Handle event messages
+                break;
+            case ERROR:
+                // Handle error messages
+                break;
+            default:
+                System.out.println("Unknown MessageType: " + messageType);
+                break;
+        }
     }
 
-    private Message handleRequest(JSONObject json) {
-        Action action = Action.valueOf(json.getInt("action"));
+    private Message handleRequest(Message msg) {
+        Action action = msg.getHeader().getAction();
+
+        assert action != null;
+        return switch (action) {
+            case LOGIN :
+            case REGISTER :
+            default : {
+                System.out.println("Unknown Action: " + action);
+                yield new Message();
+            }
+        };
+    }
+    
+    private Message handleResponse(Message msg) {
+        Action action = msg.getHeader().getAction();
 
         assert action != null;
         return switch (action) {
