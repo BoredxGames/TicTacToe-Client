425aa1d16a25d0e155b47b372308a8666e78ab4d
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-01-07 21:06:48 +0200

    refactor communication transfer formating

--- src/main/java/com/boredxgames/tictactoeclient/domain/services/communication/Message.java
+++ src/main/java/com/boredxgames/tictactoeclient/domain/services/communication/Message.java
@@ -1,26 +1,23 @@
 package com.boredxgames.tictactoeclient.domain.services.communication;
-import org.json.JSONObject;
 
+import com.google.gson.Gson;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 
 
 /**
  *
  * @author Hazem
- */
+ **/
+
 public class Message {
     private Header header = null;
-    private JSONObject data = null;
+    private String data = null;
 
     public Message() {
     }
 
-    public Message(Header header, JSONObject data) {
+    public Message(Header header, String data) {
         this.header = header;
         this.data = data;
     }
@@ -29,20 +26,25 @@ public class Message {
         return header;
     }
 
-    public JSONObject getData() {
+    public String getData() {
         return data;
     }
 
-    public JSONObject toJson() {
-        JSONObject jsonMessage = new JSONObject();
+    @Override
+    public String toString() {
+        return "Message{" + "header=" + header + ", data=" + data + '}';
+    }
 
-        jsonMessage.put("type", this.header.getMsgType());
-        jsonMessage.put("action", this.header.getAction());
-
-        jsonMessage.put("data", this.data);
-
-
-        return jsonMessage;
-
+    public static Message createMessage(MessageType type , Action action , Object data)
+    {
+        return new Message(new Header(type , action), toJson(data));
+    }
+    
+     static private String toJson(Object data)
+    {
+        Gson gson = new Gson();
+        return  gson.toJson(data);
+       
     }
 }
+
--- src/main/java/com/boredxgames/tictactoeclient/domain/services/communication/MessageRouter.java
+++ src/main/java/com/boredxgames/tictactoeclient/domain/services/communication/MessageRouter.java
@@ -8,14 +8,21 @@ package com.boredxgames.tictactoeclient.domain.services.communication;
  *
  * @author Hazem
  */
-
+import com.boredxgames.tictactoeclient.domain.managers.navigation.NavigationAction;
+import com.boredxgames.tictactoeclient.domain.managers.navigation.NavigationManager;
+import com.boredxgames.tictactoeclient.domain.managers.navigation.Screens;
+import com.boredxgames.tictactoeclient.domain.model.AuthResponseEntity;
 import com.boredxgames.tictactoeclient.domain.network.ServerConnectionManager;
-import static com.boredxgames.tictactoeclient.domain.services.communication.Action.LOGIN;
-import static com.boredxgames.tictactoeclient.domain.services.communication.Action.REGISTER;
+import static com.boredxgames.tictactoeclient.domain.services.communication.Action.LOGIN_SUCCESS;
+import static com.boredxgames.tictactoeclient.domain.services.communication.Action.USERNAME_NOT_FOUND;
+import com.boredxgames.tictactoeclient.presentation.AuthenticationController;
+import com.google.gson.Gson;
 
 public class MessageRouter {
+
     private static MessageRouter instance;
     private static ServerConnectionManager connection;
+    private Gson gson = new Gson();
     private MessageRouter() {
         connection = ServerConnectionManager.getInstance();
     }
@@ -27,57 +34,78 @@ public class MessageRouter {
         return instance;
     }
 
-    public void navigateMessage(Message message){
-       MessageType messageType = message.getHeader().getMsgType();
-       
-       assert messageType!=null; 
-       Message responseMessage;
+    public void navigateMessage(String response) {
+        Message message = gson.fromJson(response, Message.class);
+        MessageType messageType = message.getHeader().getMsgType();
+
+        assert messageType != null;
+        Message responseMessage;
         switch (messageType) {
-            case REQUEST:
-                 responseMessage = handleRequest(message);
-                break;
-            case RESPONSE:
-                 responseMessage = handleResponse(message);
-                break;
-            case EVENT:
-                // Handle event messages
-                break;
-            case ERROR:
-                // Handle error messages
-                break;
-            default:
-                System.out.println("Unknown MessageType: " + messageType);
-                break;
+            case REQUEST -> responseMessage = handleRequest(message);
+            case RESPONSE -> handleResponse(message);
+            case EVENT -> {
+            }
+            case ERROR -> handleError(message);
+            default -> System.out.println("Unknown MessageType: " + messageType);
         }
-    }
+        // Handle event messages
+            }
 
     private Message handleRequest(Message msg) {
         Action action = msg.getHeader().getAction();
 
         assert action != null;
         return switch (action) {
-            case LOGIN :
-            case REGISTER :
-            default : {
+
+            default: {
                 System.out.println("Unknown Action: " + action);
                 yield new Message();
+            }
+        };
+    }
+
+    
+    private void handleResponse(Message msg) {
+        Action action = msg.getHeader().getAction();
+           System.out.println(action);
+       
+        switch (action) {
+            case LOGIN_SUCCESS ->
+            {
+                System.out.println("Login Success");
+                AuthResponseEntity responseData = gson.fromJson(msg.getData(),AuthResponseEntity.class);
+                System.out.println(responseData);
+                NavigationManager.navigate(Screens.SECONDARY, NavigationAction.REPLACE);
+
+            }
+            case USERNAME_NOT_FOUND->{
+                System.out.println("Username not found");
+                AuthenticationController.showUserAlert("Username not found");
+                        
+            }
+            default -> {
+                System.out.println("Unknown Action: " + action);
+
             }
         };
     }
     
-    private Message handleResponse(Message msg) {
+    private void handleError(Message msg) {
         Action action = msg.getHeader().getAction();
-
-        assert action != null;
-        return switch (action) {
-            case LOGIN :
-            case REGISTER :
-            default : {
+           System.out.println(action);
+       
+        switch (action) {
+           
+            case USERNAME_NOT_FOUND->{
+                System.out.println("Username not found");
+                AuthenticationController.showUserAlert("Username not found");
+                        
+            }
+            default -> {
                 System.out.println("Unknown Action: " + action);
-                yield new Message();
+
             }
         };
     }
-
 
 }
