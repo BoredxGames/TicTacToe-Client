c9cea8adb2f0869ef7e37ea03ecc91ff72d66c79
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-01-07 21:03:57 +0200

    add authentication response entity to map response from server

---
+++ src/main/java/com/boredxgames/tictactoeclient/domain/model/AuthResponseEntity.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.boredxgames.tictactoeclient.domain.model;
+
+/**
+ *
+ * @author Hazem
+ */
+public class AuthResponseEntity {
+    private String id ;
+    private String userName ;
+    private int Score;
+
+    public AuthResponseEntity(String id, String userName, int Score) {
+        this.id = id;
+        this.userName = userName;
+        this.Score = Score;
+    }
+
+    public AuthResponseEntity() {
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setScore(int Score) {
+        this.Score = Score;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getScore() {
+        return Score;
+    }
+
+   public static AuthResponseEntity createAuthEntity(String id ,String username , int score)
+    {
+        return new AuthResponseEntity(id,username ,score );
+    }
+    
+    @Override
+    public String toString() {
+        return "AuthRequestEntity{" + "id=" + id + ", userName=" + userName + ", Score=" + Score + '}';
+    }
+    
+}
