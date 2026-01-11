0c248e30b680d392ee7c73c45a6a8d74b530d974
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-01-11 17:45:24 +0200

    feat(ai): add AI package and Minimax support

---
+++ src/main/java/com/boredxgames/tictactoeclient/domain/services/ai/BoardUtils.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.boredxgames.tictactoeclient.domain.services.ai;
+
+import com.boredxgames.tictactoeclient.domain.services.GameBoard;
+
+/**
+ *
+ * @author sheri
+ */
+public class BoardUtils {
+     public static GameBoard copy(GameBoard original) {
+        GameBoard copy = new GameBoard(original.getCurrentPlayer());
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                char cell = original.getCellValue(i, j);
+                if (cell != GameBoard.EMPTY) {
+                    copy.makeMove(i, j, cell);
+                }
+            }
+        }
+        return copy;
+    }
+}
